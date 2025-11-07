b44aab34585afd631998b543dec9b2ef45874fc1
Raphaell Monteiro <106750983+Raphaellmonteiro@users.noreply.github.com>
2025-11-06 23:05:52 -0300

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 // Use este link de teste, que é HTTPS
-var streamURL = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+var streamURL = "http://geartool.net:80/movie/nQFPp3/WPvmKG/355449.mp4";
